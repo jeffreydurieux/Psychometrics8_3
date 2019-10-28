6941de523ee7b561b65ee51798452325d98e5766
Jeffrey Durieux <durieux.jeffrey@gmail.com>
2019-10-28 13:52:41 +0100

    first commit

--- analysis.R
+++ analysis.R
@@ -1,51 +1,42 @@
-# Script copied from https://owi.usgs.gov/blog/data-munging/
+# Mon Oct 30 13:56:00 2017 ------------------------------
 
-# load libraries
-library(dplyr)
-library(dataRetrieval)
-library(lubridate)
-library(tidyr)
-library(ggplot2)
-library(viridis)
+# Jeffrey Durieux (j.durieux@fsw.leidenuniv.nl)
+# Script for plotting data in three dimensions
+# Used for assignment 8.3 of the psychometrics course
 
-# Get data for the Yahara River at Windsor in Dane County, Wisconsin
-yahara_no <- '05427718'
+# selfcontained script: one-click proof when -rgl- package (and dependencies)... 
+# ... are installed. If not: automatic installation when sourcing
 
-# define parameters of interest, and get those parameter names
-params <- c('00060', '00671', '80154', '00665')
+# if rgl not in library: install package and dependencies
+if (!is.element('plotly', installed.packages()[,1])){
+  install.packages('plotly', dep = TRUE)
+}
 
-# get daily values from NWIS
-yahara_dat <- readNWISdv(siteNumbers = yahara_no, parameterCd = params, 
-                         startDate = '1997-10-01', endDate = '2017-09-30')
+library(plotly)
 
-# rename columns using renameNWISColumns from package dataRetrieval
-yahara_dat <- renameNWISColumns(yahara_dat, 
-                                p00665 = "TP_mgL",
-                                p00671 = "Orthophosphate_mgL",
-                                p80154 = "SS_mgL")
-
-yahara_names <- names(yahara_dat)
-grep('_cd', yahara_names) # returns the index of the match
-grep('_cd', yahara_names, value = TRUE) # returns the matched elements themselves
-gsub('_cd', '_code', yahara_names)
-yahara_dat <- select(yahara_dat, -contains('_cd'))
+# The palette with black:
+cbbPalette <- c("#000000", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
 
 
-# add water year variable "waterYear" to our dataframe
-yahara_dat <- addWaterYear(yahara_dat)
+# data from assignment, adjust vectors for other examples
+rock <- c(14,11,10)
+jazz <- c(13,15,15)
+classical <- c(9,15,16)
 
-# calculate cumulative discharge for each year by first grouping by water year,
-# and then using the "cumsum" function. Add day of water year for plotting purposes.
-# These steps will build a new dataframe, with the existing information in yahara_dat
-# but with two additional columns.
-cumulative_dat <- group_by(yahara_dat, waterYear) %>%
-  mutate(cumulative_dis = cumsum(Flow), 
-         wy_doy = seq(1:n()))
+mat <- cbind(rock,jazz,classical)
+row.names(mat) <- c("speed", "basic", "complex")
 
-# visually compare cumulative discharge across years
-ggplot(cumulative_dat, aes(x = wy_doy, y = cumulative_dis, group = waterYear)) +
-  geom_line(aes(color = waterYear)) +
-  scale_color_viridis_c() +
-  scale_x_continuous(breaks = c(1, 93, 184, 275), labels = c("Oct 1", "Jan 1", "Apr 1", "July 1")) +
-  theme_bw() +
-  labs(color = "Water Year", x = "", y = "Cumulative Discharge")
+# animal: Joe 'Animal' von Karajan, the drummer
+animal <- c(11,14,13)
+mat <- cbind(mat,animal)
+
+mat <- as.data.frame(mat)
+mat <- as.data.frame(t(mat))
+mat <- cbind(mat, name = c('rock', 'jazz', 'classical', 'joe'))
+
+p <- plot_ly(data = mat, x = ~speed, y = ~basic, z = ~complex,
+             color = ~name)
+p
+
+
+
